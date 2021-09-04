1e23462195d46411c6d2da0b6da6e9e8833bd300
LGZeldner <lgzeldner@gmail.com>
2021-09-04 15:34:06 +0300

    Added response from github on RepoSearchPage

--- src/pages/RepoSearchPage/RepoSearchPage.tsx
+++ src/pages/RepoSearchPage/RepoSearchPage.tsx
@@ -2,61 +2,73 @@ import React from "react";
 
 import Button from "@components/Button";
 import Input from "@components/Input";
-import PageTitle from "@components/PageTitle";
+import Loader from "@components/Loader";
 import RepoTile from "@components/RepoTile";
 import SearchIcon from "@components/SearchIcon";
+import GitHubStore from "@store/GitHubStore";
 import { RepoItem } from "@store/GitHubStore/types";
 
 import "./RepoSearchPage.css";
 
-const repoEx = {
-  id: "string",
-  name: "string",
-  url: "string",
-  owner: {
-    id: 1,
-    login: "string",
-    avatar_url: "string",
-    url: "string"
-  },
-  private: true,
-  updated: new Date("2019-01-16"),
-  stargazers_count: 123
-};
-const repoItemsEx = [repoEx, repoEx, repoEx, repoEx, repoEx];
-export type RepoSearchPageProps = {
-  repoItems: RepoItem[];
-  isLoading?: Boolean;
-};
-let inputValue = "";
-const isLoading = false;
-const RepoSearchPage = ({ repoItems = repoItemsEx }) => {
-  //const [inputValue, setInputValue] = React.useState<string>("");
-  //const [isLoading, setIsLoading] = React.useState<boolean>(false);
-  const handleInput = (value: string) => {
-    //setInputValue(value);
-    inputValue = value;
-  }
-  const handleClick = (inputValue: string) => {
+const gitHubStore = new GitHubStore();
 
+const EXAMPLE_ORGANIZATION = 'ktsstudio';
+const QUERY = {
+  per_page: '5',
+  page: '1'
+}
+
+const RepoSearchPage = () => {
+
+  const [inputValue, setInputValue] = React.useState<string>("");
+  const handleInput = (value: string) => {
+    setInputValue(value);
   };
+
+  const [reposList, setReposList] = React.useState<RepoItem[]>([]);
+  React.useEffect(() => {
+    gitHubStore
+      .getOrganizationReposList({
+        organizationName: EXAMPLE_ORGANIZATION,
+        data: QUERY
+      })
+      .then((result) => {
+        setReposList(result.data);
+        setIsLoading(false);
+      });
+  }, []);
+
+  const [isLoading, setIsLoading] = React.useState<boolean>(true);
+
+  const [disabled, setDisabled] = React.useState(false);
+  const handleSearchButton = (inputValue: string) => {
+    setDisabled(true);
+  };
+  const handleRepoTile = (): void => {
+    // TODO: открытие боковой панели RepoBranchesDrawer
+  };
+
   return (
     <main className="light-gray-background">
       <div className="repos-list-component">
         <div className="search-bar">
           <form action="" method="get" className="search-bar__form">
-            <Input value={inputValue} placeholder="Введите название организации" onChange={(e: any) => handleInput(e.target.value)}></Input>
-            <Button onClick={() => handleClick(inputValue)} disabled={isLoading}>
+            <Input value={inputValue} placeholder="Введите название организации" onChange={handleInput}></Input>
+            <Button onClick={() => handleSearchButton(inputValue)} disabled={disabled}>
               <SearchIcon />
             </Button>
           </form>
         </div>
         <div className="repos-list">
-          <RepoTile repo={repoEx} />
-          <RepoTile repo={repoEx} />
-          <RepoTile repo={repoEx} />
-          <RepoTile repo={repoEx} />
-          <RepoTile repo={repoEx} />
+          {isLoading && <Loader name="Загружаем список..." />}
+          {reposList.map((repo) => (
+            <React.Fragment key={repo.id}>
+              <RepoTile
+                onClick={handleRepoTile}
+                item={repo}
+              />
+            </React.Fragment>
+          ))}
         </div>
       </div>
     </main>
