0ea4eb181d1ac6b850da9940114c03eb6b544db4
LGZeldner <lgzeldner@gmail.com>
2021-09-05 11:10:58 +0300

    New component: RepoBranchesDrawer

---
+++ src/components/RepoBranchesDrawer/RepoBranchesDrawer.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+
+import GitHubStore from '@store/GitHubStore';
+import { BranchItem, RepoItem } from '@store/GitHubStore/types';
+import { Drawer } from 'antd';
+
+export type RepoBranchesDrawerProps = {
+    selectedRepo: RepoItem | null;
+    onClose: () => void;
+    visible: boolean;
+};
+
+const RepoBranchesDrawer: React.FC<RepoBranchesDrawerProps> = ({
+    selectedRepo,
+    onClose,
+    visible,
+}) => {
+    const [branchesList, setBranchesList] = React.useState<BranchItem[]>([]);
+
+    React.useEffect(() => {
+        const getBranches = async () => {
+            if (selectedRepo !== null) try {
+                await new GitHubStore()
+                    .getRepoBranchesList({
+                        ownerName: selectedRepo.owner.login,
+                        repoName: selectedRepo.name,
+                    })
+                    .then((branch) => setBranchesList(branch.data));
+            } catch (err) { }
+        };
+        getBranches();
+    }, [selectedRepo]);
+
+    return <Drawer title="Ветки" placement="right" onClose={onClose} visible={visible}>
+        {branchesList.map((branch) => (
+            <React.Fragment key={branch.name}>
+                <p>{branch.name}</p>
+            </React.Fragment>
+        ))}
+    </Drawer>;
+};
+
+export default RepoBranchesDrawer;
